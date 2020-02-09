5573db1facfef10a8e49a9b935f7f12341c537f6
Christian Young <cdyoung1@uci.edu>
2020-02-09 13:41:16 -0800

    scratch

--- scraper.py
+++ scraper.py
@@ -4,58 +4,58 @@ from urllib import robotparser
 import shelve
 
 # Additional packages
-from bs4 import BeautifulSoup
+from lxml import html
 
+# Setting up robots
 robots = dict()
+robot_urls = ["https://www.ics.uci.edu/robots.txt","https://www.cs.uci.edu/robots.txt",
+    "https://www.informatics.uci.edu/robots.txt","https://www.stat.uci.edu/robots.txt", "https://today.uci.edu/robots.txt"]
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    scraped_links = [link for link in links if is_valid(link)]
-    # if "url_set" not in urls:
-    #     urls["url_set"] = set(scraped_links)
-    # else:
-    #     urls["url_set"].update(set(scraped_links))
-    # urls.close()
-    return scraped_links
+    return [link for link in links if is_valid(link)]
 
-def createRobot(url):
-    if url not in robots:
-        rp = robotparser.RobotFileParser(url)
-        rp.read()
-        robots[url] = rp
-    return robots[url]
+# def createRobot(url):
+#     if url not in robots:
+#         rp = robotparser.RobotFileParser()
+#         rp.set_url(url)
+#         rp.read()
+#         robots[url] = rp
+#     return robots[url]
 
-def extract_raw_links(raw):
-    links = []
-    soup = BeautifulSoup(raw, features="lxml")
-    for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-        links.append(link)
-    return links
+# def extract_raw_links(raw):
+#     links = []
+#     soup = BeautifulSoup(raw, features="lxml")
+#     for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
+#         links.append(link)
+#     return links
 
 def extract_next_links(url, resp):
-    urls = shelve.open("urls.shelve")
+    parse = urlparse(url)
+    print("Parsed url:", parse)
+    return list()
+    # urls = shelve.open("urls.shelve")
     # Implementation requred.
-    new_links = set()
-
-    try:
-        parse = urlparse(url)
-        robot = createRobot(parse.scheme + "://" + parse.netloc + "/robots.txt")
-        print(parse)
-        if 200 <= resp.status <= 599:
-            raw = resp.raw_response
-            raw_links = extract_raw_links(raw.text)
-            print(raw_links)
-            for link in raw_links:
-                l = link.get("href")
-                if robot.can_fetch("*", l):
-                    new_links.add(l)
-                    if urls.get(l) == None:
-                        urls[l] = 1
-    except Exception as e:
-        print("Except", e)
-    finally:
-        urls.close()
+    # new_links = set()
+    # try:
+    #     parse = urlparse(url)
+    #     robot = createRobot(parse.scheme + "://" + parse.netloc + "/robots.txt")
+    #     print(parse)
+    #     if 200 <= resp.status <= 599:
+    #         raw = resp.raw_response
+    #         raw_links = extract_raw_links(raw.text)
+    #         for link in raw_links:
+    #             l = link.get("href")
+    #             if robot.can_fetch("*", l):
+    #                 new_links.add(l)
+    #                 if urls.get(l) == None:
+    #                     urls[l] = 1
+    # except Exception as e:
+    #     print("Except", e)
+    # finally:
+    #     urls.close()
         
-    return list(new_links)
+    # return list(new_links)
 
 def is_valid(url):
     try:
@@ -63,9 +63,9 @@ def is_valid(url):
         if parsed.scheme not in set(["http", "https"]):
             return False
         print("Parsed", parsed)
-        validPath = r".*\.(ics|cs|informatics|stat)\.uci\.edu/.*|today\.uci\.edu/department/information_computer_sciences/.*"
-        return re.match(validPath, url.lower()) and not re.match(
-        # return not re.match(
+        # validPath = r".*\.(ics|cs|informatics|stat)\.uci\.edu/.*|today\.uci\.edu/department/information_computer_sciences/.*"
+        # return re.match(validPath, url.lower()) and not re.match(
+        return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
