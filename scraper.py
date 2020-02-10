44116dce264515e6d6ae73987938d902169e210a
Christian Young <cdyoung1@uci.edu>
2020-02-10 10:45:44 -0800

    testing is_valid

--- scraper.py
+++ scraper.py
@@ -65,24 +65,49 @@ def check_robot(url, parsed):
 def is_valid(url):
     try:
         parsed = urlparse(url)
-        
+
+        # Check scheme of url
+        if parsed.scheme not in set(["http", "https"]):
+            return False
+
+        # Check if url has already been visited
         if url in visited:
             return False
 
+        # Check if url contains any trap phrases (eg. pdf, calendar, events)
         for trap in disallowed:
             if trap in url:
                 return False
-
-        if parsed.scheme not in set(["http", "https"]):
-            return False
 
         # Create and check robots.txt
         if not check_robot(url, parsed):
             return False
 
+        # Check if url is too long
         if len(url) > 175:
             return False
+        print("-------CHECKING---------")
+        print("Checking is_valid for:",url)
+        print("Parsed:",parsed)
+        print("-------CHECKING---------")
 
+
+        # Check that url does not contain invalid types in middle or in query
+        valid_mid_and_query = r".*\/(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz).*$"
+
+        if re.match(valid_mid_and_query, parsed.query.lower()) or re.match(valid_mid_and_query, parsed.path.lower()):
+            return False
+        
+        print("------QUERY AND PATH ARE FINE---------")
+
+        # Match specified domains
         valid_domains = re.match(r".*\.ics|cs|informatics|stat\.uci\.edu(\/.*)*" + r"today\.uci\.edu\/department\/information_computer_sciences(\/.*)*", parsed.netloc.lower()) 
 
         return valid_domains and not re.match(
