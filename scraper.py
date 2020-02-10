b3272640028e7dcdf01610362643b785febd00e4
Christian Young <cdyoung1@uci.edu>
2020-02-10 10:51:03 -0800

    testing is_valid

--- scraper.py
+++ scraper.py
@@ -93,14 +93,14 @@ def is_valid(url):
 
 
         # Check that url does not contain invalid types in middle or in query
-        valid_mid_and_query = r".*\/(css|js|bmp|gif|jpe?g|ico"
+        valid_mid_and_query = (r".*\/(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz).*$"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz).*$")
 
         # hi = r".*\/(css|js|bmp|gif|jpe?g|ico"
         #     + r"|png|tiff?|mid|mp2|mp3|mp4"
