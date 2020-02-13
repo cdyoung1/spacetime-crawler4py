04a6bf7ec2578f4d1fab73dae4da613787d9c433
Christian Young <cdyoung1@uci.edu>
2020-02-13 01:26:34 -0800

    fixed largest page word count counter

--- scraper.py
+++ scraper.py
@@ -12,6 +12,7 @@ visited = set()
 robots = dict()
 wordsDict = dict()
 stats = {"longest-page-count": 0, "longest-page" : ""}
+pageWordCounts = dict()
 subdomains = dict()
 SimIndex = SimhashIndex([])
 interval = 1
@@ -24,6 +25,7 @@ def scraper(url, resp):
     global stats
     global wordsDict
     global interval
+    global pageWordCounts
 
     scraped_links = set()
     links = extract_next_links(url, resp)
@@ -62,7 +64,10 @@ def scraper(url, resp):
                     subdomain_file.write(str(kv[0]) + ", " + str(kv[1]) + "\n")
 
             with open("stats.txt", "w") as stats_file:
-                stats_file.write(str(stats))
+                stats_file.write(str(stats) + "\n")
+                stats_file.write("================================")
+                for kv in sorted(pageWordCounts.items(), key = lambda x : x[1], reverse = True):
+                    stats_file.write(str(kv[0]) + " -> " + str(kv[1]) + "\n")
 
             if len(wordsDict) >= 50:
                 with open("words.txt", "w") as words_file:
@@ -105,6 +110,7 @@ def fix_relative_url(url, base_parse):
 
 def tokenize(url, html):
     global wordsDict
+    global pageWordCounts
     global stats
     stopwords = {"about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's""me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
 
@@ -125,6 +131,9 @@ def tokenize(url, html):
                 wordsDict[word] = 1
             else:
                 wordsDict[word] += 1
+
+    pageWordCounts[url] = wordCount
+    
     if stats["longest-page-count"] < wordCount:
         stats["longest-page-count"] = wordCount
         stats["longest-page"] = url  
