c59f805be0d65112c00d5218b5ca36b78ce8b661
Christian Young <cdyoung1@uci.edu>
2020-02-12 23:58:45 -0800

    added report info

--- scraper.py
+++ scraper.py
@@ -28,7 +28,7 @@ def scraper(url, resp):
     scraped_links = set()
     links = extract_next_links(url, resp)
 
-    if resp.raw_response !== None:
+    if resp.raw_response != None:
         tokenize(url, resp.raw_response.content)
 
     print()
