210868984837fe18f335ef31c6320e7304d58d40
Christian Young <cdyoung1@uci.edu>
2020-02-17 17:38:26 -0800

    Final commit

--- scraper.py
+++ scraper.py
@@ -112,16 +112,8 @@ def fix_relative_url(url, base_parse):
 
     return fixed
 
-# Parse url and its html content for statistics (word count, longest page/word count)
-def tokenize(url, html):
-    global wordsDict, pageWordCounts, stats
-
-    # Stopwords from https://www.ranks.nl/stopwords - Default English Stopwords
-    stopwords = {"about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's""me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
-
+def getText(html):
     words = []
-    wordCount = 0
-
     parser = etree.HTMLParser()
     et = etree.parse(StringIO(html), parser)
     r = et.getroot()
@@ -132,7 +124,20 @@ def tokenize(url, html):
                 words.append(i.text.lower())
 
     # Replace anything that is not A-Z, a-z, 0-9, or " ' " with a space and split it
-    text = ' '.join(words)
+    return ' '.join(words)
+
+# Parse url and its html content for statistics (word count, longest page/word count)
+def tokenize(url, html):
+    global wordsDict, pageWordCounts, stats
+
+    # Stopwords from https://www.ranks.nl/stopwords - Default English Stopwords
+    stopwords = {"about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's""me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
+
+    words = []
+    wordCount = 0
+
+    # Replace anything that is not A-Z, a-z, 0-9, or " ' " with a space and split it
+    text = getText(html)
     words = re.sub(r"[^a-zA-Z0-9\']", " ", text).split()
 
     for word in words:
@@ -188,7 +193,7 @@ def extract_next_links(url, resp):
             return []
 
         # Generate a Simhash object based off page's text
-        url_sim = Simhash(bs.get_text())
+        url_sim = Simhash(getText(resp.raw_response.content.decode('utf-8')))
 
         # Check for near dupes, and if so, return an empty list
         if SimIndex.get_near_dups(url_sim):
